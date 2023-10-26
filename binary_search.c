d1a4e23186706d5f58d36588ffd586c4a4b266d0
nianhai1212 <42083021+nianhai1212@users.noreply.github.com>
2023-10-26 15:17:20 +0800

    Update binary_search.c

--- binary_search.c
+++ binary_search.c
@@ -20,25 +20,31 @@ int main()
 		array[index] = index;
 		printf("element is %d\n",array[index]);
 	}
-
+	/*full array search*/
 	//for(index = 0; index < SIZE; index++) {
 	//	if(num == array[index]) {
 	//		printf("Find %d in array %d\n",num,index);
 	//	}
 	//}
 	//
+
+	/*binary search*/
 	left = 0;
 	right = SIZE-1;
 	mid = (left+right)/2;
 
 	not_found = 0;
 	while(num != array[mid]) {
+		/* if data is out of range the array then quit and report not found */
 		if(num > array[right] || num < array[left]) {
 			not_found = 1;
 			printf("given data out of range of array range!\n");
 			break;
 		}
 
+		/* else if data is in the range of the arary then change the left and right and mid position to
+		 * find the target number
+		*/
 		if(num > array[mid]) {
 			left = mid;
 			mid = (left+right)/2;
