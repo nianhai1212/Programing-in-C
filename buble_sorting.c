f2aeb666f312f9b222fc298c30e53b8ebcc84d16
nianhai1212 <42083021+nianhai1212@users.noreply.github.com>
2023-10-27 20:16:00 +0800

    Update buble_sorting.c
    add the ordered flag to enchece the efficency

--- buble_sorting.c
+++ buble_sorting.c
@@ -12,6 +12,7 @@ int main()
 	int i = 0; 
 	int j = 0;
 	int tmp = 0;
+	int flag = 0;//add flag to enhance efficency 
 	for(index = 0; index < ARR_SIZE; index++) {
 		random = rand()%255;
 		arr[index] = random;
@@ -19,12 +20,17 @@ int main()
 	}
 
 	for(i = 0; i < ARR_SIZE-1; i++) {
+		flag = 1;//asume the array is ordered -- set the flag to be 1
 		for(j = 0; j < ARR_SIZE - i - 1; j++) {
 			if(arr[j] < arr[j+1]) {
 				tmp = arr[j];
 				arr[j] = arr[j+1];
 				arr[j+1] = tmp;
+				flag = 0;//if swap occured means that the array is not ordered and swaped for ordering clear the flag
 			}
+		}
+		if (1== flag) { // after a round compare and swap if the flag is 1 still means that the array is ordred already and stop compre and swap
+			break;
 		}
 	}
 
